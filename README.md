d9acc990cb41e2cf4f9170a08743ca9d93c1160f
LYcwed <ylechevalier@gmail.com>
2018-02-10 23:14:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# lycwed-one-signal-notification-icons
+
+This plugin add hook to handle icons fo one signal in the project.
+
+## Install with npm
+
+```$ cordova plugin add lycwed-one-signal-notification-icons --save```
---
+++ src/prepareNoticationIcons.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+var rootdir = process.argv[2];
+var filestocopy = [
+    {
+        "resources/notification/drawable-hdpi/ic_stat_icon.png":
+        "platforms/android/res/drawable-hdpi/ic_stat_onesignal_default.png"
+    },
+    {
+        "resources/notification/drawable-mdpi/ic_stat_icon.png":
+        "platforms/android/res/drawable-mdpi/ic_stat_onesignal_default.png"
+    },
+    {
+        "resources/notification/drawable-xhdpi/ic_stat_icon.png":
+        "platforms/android/res/drawable-xhdpi/ic_stat_onesignal_default.png"
+    },
+    {
+        "resources/notification/drawable-xxhdpi/ic_stat_icon.png":
+        "platforms/android/res/drawable-xxhdpi/ic_stat_onesignal_default.png"
+    },
+    {
+        "resources/notification/drawable-xxxhdpi/ic_stat_icon.png":
+        "platforms/android/res/drawable-xxxhdpi/ic_stat_onesignal_default.png"
+    },
+    {
+        "resources/android/icon/drawable-xxxhdpi-icon.png":
+        "platforms/android/res/drawable-xxxhdpi/ic_onesignal_large_icon_default.png"
+    }
+];
+
+if (fs.existsSync('./platforms/android/')) {
+    for (var i = 0, l = filestocopy.length; i < l; i++) {
+        var file = filestocopy[i];
+
+        for (var key in file) {
+            var val = file[key];
+            var srcfile = path.join(rootdir, key);
+            var destfile = path.join(rootdir, val);
+            var destdir = path.dirname(destfile);
+            if (fs.existsSync(srcfile)) {
+                if (!fs.existsSync(destdir)) {
+                    fs.mkdirSync(destdir);
+                }
+                console.log("copying " + key + " to " + val);
+                fs.createReadStream(srcfile).pipe(fs.createWriteStream(destfile));
+            }
+        }
+    }
+}
